811becb2bc5600f74301ba5b0dfd201d6a64f638
LocPhanRevature <loc.phan@revature.net>
2021-12-30 09:01:14 -0600

    new

--- src/main/java/dev/chirp/app/controller/AppController.java
+++
@@ -1,8 +0,0 @@
-package dev.chirp.app.controller;
-
-import dev.chirp.app.controller.controllers.UserController;
-
-public class AppController {
-
-    public UserController userController = new UserController();
-}
