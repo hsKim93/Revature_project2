14400ba10ddb680a8752a7443839c3d51938c45d
Hyungsuk Kim <76229107+hsKim93@users.noreply.github.com>
2022-01-01 04:20:24 +0900

    Revert "Sprint 1"

--- src/main/java/dev/chirp/dao/implementations/CommentDAOImp.java
+++ src/main/java/dev/chirp/dao/implementations/CommentDAOImp.java
@@ -1,56 +1,43 @@
 package dev.chirp.dao.implementations;
 
-import dev.chirp.customexceptions.CommentNotFound;
 import dev.chirp.dao.interfaces.CommentDAO;
 import dev.chirp.entities.Comment;
 import dev.chirp.utility.ConnectionDB;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CommentDAOImp implements CommentDAO {
 
     @Override
-    public Comment createComment(Comment comment) {
-        try (Connection connection = ConnectionDB.createConnection()){
-           String sql = "insert into project2.comments values(default, ?, ?, ?, default)";
-            assert connection != null;
-            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-           preparedStatement.setInt(1, comment.getPostId());
-           preparedStatement.setInt(2, comment.getUserId());
-           preparedStatement.setString(3, comment.getCommentContent());
-           preparedStatement.execute();
-           ResultSet resultSet = preparedStatement.getGeneratedKeys();
-           resultSet.next();
-           comment.setCommentId(resultSet.getInt("comment_id"));
-           return comment;
-        } catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
+    public boolean createComment(Comment comment) {
+//        try (){
+//            return true;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
         return false;
     }
 
     @Override
     public Comment getCommentById(int commentId) {
         try (Connection connection = ConnectionDB.createConnection()){
-            String sql = "select * from project2.comments where comment_id = ?";
-            assert connection != null;
+            String sql = "select * from comments where comment_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, commentId);
             ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()) {
-                return new Comment(
+            resultSet.next();
+            Comment comment = new Comment(
                     resultSet.getInt("comment_id"),
                     resultSet.getInt("post_id"),
-                    resultSet.getInt("user_id"),
                     resultSet.getString("comment_content"),
                     resultSet.getString("date")
-                );
-            } else {
-                throw new CommentNotFound("Comment not found");
-            }
+            );
+            return comment;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
@@ -59,74 +46,16 @@ public class CommentDAOImp implements CommentDAO {
 
     @Override
     public List<Comment> getCommentsByPostId(int postId) {
-        try (Connection connection = ConnectionDB.createConnection()){
-            String sql = "select * from project2.comments where post_id = ?";
-            assert connection != null;
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, postId);
-            ResultSet resultSet = statement.executeQuery();
-            List<Comment> comments = new ArrayList<>();
-            while(resultSet.next()){
-                Comment comment = new Comment(
-                        resultSet.getInt("comment_id"),
-                        resultSet.getInt("post_id"),
-                        resultSet.getInt("user_id"),
-                        resultSet.getString("comment_content"),
-                        resultSet.getString("date")
-                );
-                comments.add(comment);
-            }
-            return comments;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return null;
     }
 
     @Override
     public List<Comment> getAllComments() {
-        try (Connection connection = ConnectionDB.createConnection()){
-            String sql = "select * from project2.comments";
-            assert connection != null;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            List<Comment> comments = new ArrayList<>();
-            while(resultSet.next()){
-                Comment comment = new Comment(
-                        resultSet.getInt("comment_id"),
-                        resultSet.getInt("post_id"),
-                        resultSet.getInt("user_id"),
-                        resultSet.getString("comment_content"),
-                        resultSet.getString("date")
-                );
-                comments.add(comment);
-            }
-
-            return comments;
-
-        } catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
+        return null;
     }
 
     @Override
     public boolean deleteCommentById(int commentId) {
-        try(Connection connection= ConnectionDB.createConnection()){
-            String sql = "delete from project2.comments where comment_id = ? returning comment_id";
-            assert connection != null;
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, commentId);
-            ResultSet returned =statement.executeQuery();
-            if (returned.next()){
-                return true;
-            }
-            else {
-                throw new CommentNotFound("Comment not found");
-            }
-        } catch (SQLException e){
-            e.printStackTrace();
-            return false;
-        }
+        return false;
     }
 }
