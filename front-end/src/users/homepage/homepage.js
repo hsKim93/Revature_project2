5784ae064deee2b93c15b531fb36d5e86157f231
Irfan Uludag <ozgr329@gmail.com>
2022-01-07 12:23:34 -0500

    Added create posts, create comment, like/unlike, fixed status codes

--- front-end/src/users/homepage/homepage.js
+++ front-end/src/users/homepage/homepage.js
@@ -1,10 +1,12 @@
 const url = "http://localhost:8080/";
 
-async function createPost(getP, method) {
+document.getElementById("sendPostButton").addEventListener("click",createPost);
+
+async function createPost() {
   const rqjson = {
     postId: 0,
     userId: sessionStorage.getItem("userId"),
-    content: "Testing postman",
+    content: document.getElementById("postInput").value,
     date: "",
   };
   let response = await fetch(url + "post/create", {
@@ -17,78 +19,33 @@ async function createPost(getP, method) {
     body: JSON.stringify(rqjson),
   });
   let responseBody = await response.json();
-  return responseBody;
-}
-
-async function getPostbyUserId(id) {
-  let response = await fetch(url + `post/byUserId/${id}`, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-  });
-  let responseBody = await response.json();
-  return responseBody;
-}
-
-async function getPostbyId(id) {
-  let response = await fetch(url + `post/byPostId/${id}`, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-  });
-  let responseBody = await response.json();
-  return responseBody;
-}
-
-async function getLikesByPostId(id) {
-  let response = await fetch(url + `post/likes/${id}`, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-  });
-  let responseBody = await response.json();
-  return responseBody;
-}
-
-async function getUserById(id) {
-  let response = await fetch(url + `user/${id}`, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-  });
-  let responseBody = await response.json();
-  return responseBody;
-}
-
-async function getCommentsByPostId(id) {
-    let response = await fetch(url + `comment/postId/${id}`, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow",
-    });
-    let responseBody = await response.json();
-    return responseBody;
+  if(responseBody == true){
+    document.getElementById("postInfo").innerHTML = `Post sent`
+  }else{
+    document.getElementById("postInfo").innerHTML = `Post could not be sent`
   }
+}
+
+async function getPostModulebyUserId(id) {
+  let response = await fetch(url + `post/module/${id}`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+  });
+  let responseBody = await response.json();
+  return responseBody;
+}
+
+
+
 
 async function likePost(userId, postId) {
   const rqjson = {
     userId: userId,
-    PostId: postId,
+    postId: postId,
   };
 
   let response = await fetch(url + "post/like", {
@@ -101,13 +58,18 @@ async function likePost(userId, postId) {
     body: JSON.stringify(rqjson),
   });
   let responseBody = await response.json();
-  return responseBody;
+  if(responseBody === false){
+    unlikePost(userId,postId)
+  }
+  else{
+    document.getElementById("likeInfo"+postId).innerHTML = `Post liked`
+  };
 }
 
 async function unlikePost(userId, postId) {
   const rqjson = {
     userId: userId,
-    PostId: postId,
+    postId: postId,
   };
 
   let response = await fetch(url + "post/unlike", {
@@ -120,57 +82,59 @@ async function unlikePost(userId, postId) {
     body: JSON.stringify(rqjson),
   });
   let responseBody = await response.json();
-  return responseBody;
+  if(responseBody === true){
+    document.getElementById("likeInfo"+postId).innerHTML = `Post unliked`;
+  }
+  else{
+    document.getElementById("likeInfo"+postId).innerHTML = `Post could not be unliked`;
+  };
 }
 
-async function getFollowingByUserId(id) {
-  let response = await fetch(url + `following/${id}`, {
-    method: "GET",
+async function createComment1(postId) {
+  const rqjson = {
+    commentId:"0", 
+    postId: Number(postId), 
+    userId:Number(sessionStorage.getItem("userId")),
+    commentContent: document.getElementById("inputComment"+postId).value,
+    date: ""
+  };
+
+  let response = await fetch(url + "comment/create", {
+    method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
     redirect: "follow",
+    body: JSON.stringify(rqjson),
   });
   let responseBody = await response.json();
-  return responseBody;
+  if(response.status == 201){
+    document.getElementById("commentInfo"+postId).innerHTML = `Comment sent`
+  }else{
+    document.getElementById("commentInfo"+postId).innerHTML = `Comment could not be sent`
+  }
 }
 
 async function loadPostModule() {
-  let userId = 8001;
-  let following = await getFollowingByUserId(userId);
-
-  let postList = [];
-  for (let users of following) {
-    userPosts = await getPostbyUserId(users);
-
-    console.log(userPosts);
-    for (let post of userPosts) {
-      let user = await getUserById(post.userId);
-      post.fullName = user.firstName + " " + user.lastName;
-      post.date= post.date.slice(0,-10);
-      post.userName = user.userName;
-      post.comments = await getCommentsByPostId(post.postId);
-      post.likes= await getLikesByPostId(post.postId);
-      postList.push(post);
-    }
-  }
-  postList.sort(function (a, b) {
+  let userId = sessionStorage.getItem("userId");
+  let posts = await getPostModulebyUserId(userId);
+  posts.sort(function (a, b) {
     return a.postId - b.postId;
   });
-  console.log(postList);
-
-  for (let a of postList) {
-
+ if(posts.length == 0){
+  document.getElementById("accordionHome").innerHTML += `<h1>NO POSTS</h1>`
+ }
+  for(let a of posts){
     document.getElementById("accordionHome").innerHTML += 
       `
     <div class="accordion-item bg-black border-dark p-1">
     <div class="accordion-header" id="heading` +
       a.postId +
       `">
-      <img src="postIcon.png" style="height:2em;width:2em;">
+      <img src="../../resources/postIcon.png" style="height:2em;width:2em;">
       <a style="font-weight: bold;">` +
-      a.fullName +
+      a.firstName + " " + a.lastName +
       `</a>
      <span style=" font-size: 0.8em;color:grey;">` +
       a.date+
@@ -180,12 +144,13 @@ async function loadPostModule() {
         <p>` +
       a.content +
       `</p>
-        <p><a class="btn btn-sm text-white" type="button">Like (`+a.likes+`)<a><span>  </span><a class="btn text-white btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` +
+        <p><a class="btn btn-sm text-white" id="likeButton`+a.postId+`" type="button" onclick="likePost(`+a.userId+`,`+a.postId+`)"><span id="likeStatus"> Like</span>(`+a.likes+`)</a><span>  </span><a class="btn text-white btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` +
       a.postId +
       `" aria-expanded="true" aria-controls="collapse` +
       a.postId +
-      `"> Comments (`+a.comments.length +`)
+      `"> Comments (`+a.comment.length +`)
         </a></p>
+        <p id="likeInfo`+a.postId+`"></p>
     </div>
     <div id="collapse` +
       a.postId +
@@ -198,17 +163,19 @@ async function loadPostModule() {
       <div class="row justify-content-evenly">
       <div class="col-lg-10 col-md-10 col-sm-8">
 
-      <textarea class="form-control form-control-sm bg-black text-white" style="display:inline-block;" id="exampleFormControlTextarea1"  maxlength = "500" rows="3"></textarea>
+      <textarea class="form-control form-control-sm bg-black text-white" style="display:inline-block;" id="inputComment`+a.postId+`"  maxlength = "500" rows="3"></textarea>
        </div>
-      <div class="col-lg-2 col-md-2 col-sm-4"><button type="button" style="display:inline-block;" class="btn btn-outline-info align-self-center ">Chirp</button>   </div>
+      <div class="col-lg-2 col-md-2 col-sm-4" id="commentSection`+a.postId+`">   <button class="btn btn-outline-info align-self-center" onclick="createComment1(`+a.postId+`)">Chirp</button></div>
       </div>
+      <p id="commentInfo`+a.postId+`"></p>
     </div>
       </div>
     </div>
   </div>`;
-  for(let b of a.comments){
-      let user = await getUserById(b.userId)
-      document.getElementById("comment" + b.postId).innerHTML +=  `<li class="list-group-item bg-black text-white"><img src="commentIcon.png" style="height:1.5em;width:1.5em;"><span class="font-weight-bold" style="color:rgb(69, 155, 212);">  @`+user.userName+`  </span>`+ b.commentContent+`</li>`
+
+
+  for(let b of a.comment){
+      document.getElementById("comment" + b.postId).innerHTML +=  `<li class="list-group-item bg-black text-white"><img src="../../resources/commentIcon.png" style="height:1.5em;width:1.5em;"><span class="font-weight-bold" style="color:rgb(69, 155, 212);">  @`+b.userName+`  </span>`+ b.commentContent+`</li>`
 
   }
   }
