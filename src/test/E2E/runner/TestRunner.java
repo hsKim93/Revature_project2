03ffa7380d330bf08c7319b4a2da449d8d577c8a
LocPhanRevature <loc.phan@revature.net>
2022-01-11 15:44:19 -0600

    add

--- src/test/E2E/runner/TestRunner.java
+++ src/test/E2E/runner/TestRunner.java
@@ -1,9 +1,6 @@
 package E2E.runner;
 
 import E2E.poms.LoginPage;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,8 +10,7 @@ import org.testng.annotations.BeforeClass;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="classpath:features", glue="E2E.steps")
+
 
 
 public class TestRunner {
