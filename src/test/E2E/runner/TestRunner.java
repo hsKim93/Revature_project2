63150fb659805b84ca4a0462c442f30b2836cf7d
Hyungsuk Kim <76229107+sesame0717@users.noreply.github.com>
2022-01-11 13:34:52 -0800

    added user POMS

--- src/test/E2E/runner/TestRunner.java
+++ src/test/E2E/runner/TestRunner.java
@@ -1,6 +1,9 @@
 package E2E.runner;
 
 import E2E.poms.LoginPage;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,6 +13,8 @@ import org.testng.annotations.BeforeClass;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(features="classpath:features", glue="E2E.steps")
 public class TestRunner {
     public static WebDriver driver;
     public static LoginPage loginPage;
